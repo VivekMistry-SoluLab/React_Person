117a2e1fa902738a9eeb021b5d5dd8be894d8bf6
Vivek <vivekdmistry1999@gmail.com>
2021-02-07 19:06:32 +0530

    creation new branch and add App.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,86 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = (props) => {
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-  const [personsState,setPersonsState] = useState({
-    persons : [
-      {name : "Vivek", age: "21"},
-      {name : "Neel", age: "24"},
-      {name : "Ayush", age: "25"},
-    ]
-  })
+  switchNameHandler = ( newName ) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
+  }
 
-  const swichHandler =()=> {
-    setPersonsState({
-      persons : [
-      {name : "Megh", age: "22"},
-      {name : "Hemil", age: "23"},
-      {name : "Meet", age: "20"},
-    ]
-    })
-   }
-  // //this is not work
-  // const nameChangeHandler = (props) => {
-  //   const nameChange = useState({
-  //     persons : [
-  //      {name : "Vivek", age: "21"},
-  //      {name : event.target.value, age: "24"},
-  //      {name : "Ayush", age: "25"},
-  //    ]
-  //   })
-  // }
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
 
-   const style = {
-     backgroundColor:'white',
-     font:'inherit',
-     border: '1px solid blue',
-     padding:'8px'
-   };
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind( this, 'Max!' )}
+            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button 
-        style={style}
-        onClick={swichHandler}>Switch Name</button>
-        <Person 
-        name={personsState.persons[0].name} 
-        age={personsState.persons[0].age}> 
-          My Hobby is Cricket\
-        </Person>
-
-        <Person 
-        name={personsState.persons[1].name} 
-        age={personsState.persons[1].age}
-        // changed={nameChangeHandler}
-        />
-
-        <Person 
-        name={personsState.persons[2].name} 
-        age={personsState.persons[2].age} />
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
 export default App;
