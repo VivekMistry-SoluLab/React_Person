b655fd60a2cf687e3d20649b508f5d592e06ad14
Vivek <vivekdmistry1999@gmail.com>
2021-02-08 10:00:36 +0530

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
           <Person
             name={this.state.persons[1].name}
             age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind( this, 'Max!' )}
+            click={this.switchNameHandler.bind( this, 'Vivek!' )}
             changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
           <Person
             name={this.state.persons[2].name}
